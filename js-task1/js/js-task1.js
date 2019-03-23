9ee3a584945baa0f1893ffef940103107b7117c5
lurenxie <34667351+lurenxie@users.noreply.github.com>
2019-03-23 18:16:35 +0800

    重做任务一

--- js-task1/js/js-task1.js
+++ js-task1/js/js-task1.js
@@ -1,33 +1,48 @@
-var changeVar1 = null;
-var changeVar2 = null;
-var changeVar3 = null;
+var getboxelement = document.getElementsByClassName('one');
+var nineArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var changeVar = null;
+//从array数组中获取num个随机元素并输出
+function getRandomArray(array, num) {
+    var sliceArray = array.slice(0);
+    var lengthArray = array.length;
+    var min = lengthArray - num,returnArray = [],index,i = 0;
+    while(lengthArray --> min) {
+        index = Math.floor((lengthArray+1)*Math.random());
+        returnArray[i] = sliceArray[index];
+        sliceArray.splice(index,1);
+        i++;
+    }
+    return(returnArray);
+}
+//获取随机颜色
 function getColot() {
     var r=Math.floor(Math.random()*256);
     var g=Math.floor(Math.random()*256);
     var b=Math.floor(Math.random()*256);
     return "rgb("+r+','+g+','+b+")";//所有方法的拼接都可以用ES6新特性`其他字符串{$变量名}`替换
 }
-function changeColor() {
-    var numberTen =  Math.ceil(Math.random()*9);
-    var nodeId = "one" + numberTen;
-    var nodeName = document.getElementById(nodeId);
-    nodeName.style.background = getColot();
-    numberTen = null;
-    nodeId = null;
-    nodeName = null;
+//随机改变三个方块颜色
+function runChangecolor() {
+    var radonmArray = getRandomArray(nineArray, 3);
+    // alert(radonmArray);
+    getboxelement[radonmArray[0]].style.backgroundColor = getColot();
+    getboxelement[radonmArray[1]].style.backgroundColor = getColot();
+    getboxelement[radonmArray[2]].style.backgroundColor = getColot();
 }
-function loadFooterbtn1() {
-    changeVar1=setInterval("changeColor()", 1000);
-    changeVar2=setInterval("changeColor()", 1000);
-    changeVar3=setInterval("changeColor()", 1000);
-}
-function loadFooterbtn2() {
-    clearInterval(changeVar1);
-    clearInterval(changeVar2);
-    clearInterval(changeVar3);
-    for (i=1; i<=9 ; i++)  {
-        var nodeId = "one" + i;
-        var nodeName = document.getElementById(nodeId);
-        nodeName.style.background = '#FF9900';
+//恢复所有格子原色
+function recoverColor() {
+    for (var i= 0 ;i < getboxelement.length ;i++) {
+        getboxelement[i].style.backgroundColor = '#FF9900';
     }
 }
+//给id为footer-btn1的dom节点添加按键事件句柄，使鼠标按下该节点时执行匿名函数
+document.getElementById('footer-btn1').addEventListener('click',function() {
+    clearInterval(changeVar);
+    recoverColor();
+    changeVar = setInterval("runChangecolor()", 200);
+});
+//给id为footer-btn2的dom节点添加按键事件句柄，使鼠标按下该节点时执行匿名函数
+document.getElementById('footer-btn2').addEventListener('click',function () {
+    clearInterval(changeVar);
+    recoverColor();
+});
